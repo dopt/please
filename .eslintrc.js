dopt/please

commit 734c8f5d860c8baad2d8a871af3402cddc90eebb

refactor(@dopt/please): migrate to @pkg-tools/pkg-tools toolchain for build-related tasks

packages/@dopt/please/.eslintrc.cjs
@@ -1,4 +1,0 @@
-const { eslint } = require('@dopt/config');
-module.exports = {
-  ...eslint,
-};

packages/@dopt/please/build.config.ts
@@ -1,5 +1,0 @@
-import { external } from "@dopt/pkg-build";
-
-export default external.node({
-  entries: ["src/index"],
-});

packages/@dopt/please/pkg-tools.config.ts
@@ -1,0 +1,5 @@
+import { config, build } from "@pkg-tools/pkg-tools";
+
+export default config.definePkgToolsConfig({
+  build: build.config.node({}),
+});

commit aee65799c76dd5c7d3e2d3b16c82fbfd059bd412

fix: add .eslintrc.cjs files to packages
Fixes https://github.com/pkg-tools/pkg-tools/issues/26 in this repo, introduced by
https://github.com/dopt/please/commit/734c8f5d860c8baad2d8a871af3402cddc90eebb

.eslintrc.js
@@ -1,4 +1,0 @@
-const { eslint } = require('@dopt/config');
-module.exports = {
-  ...eslint,
-};

packages/@dopt/please-function/.eslintrc.cjs
@@ -1,0 +1,2 @@
+const pkgToolsConfig = require('./pkg-tools.config')
+module.exports = pkgToolsConfig.lint;

packages/@dopt/please-parser/.eslintrc.cjs
@@ -1,0 +1,2 @@
+const pkgToolsConfig = require('./pkg-tools.config')
+module.exports = pkgToolsConfig.lint;

packages/@dopt/please/.eslintrc.cjs
@@ -1,0 +1,2 @@
+const pkgToolsConfig = require('./pkg-tools.config')
+module.exports = pkgToolsConfig.lint;

packages/@dopt/please/prettier.config.mjs
@@ -1,2 +1,2 @@
 import pkgToolsConfig from './pkg-tools.config';
-return pkgToolsConfig.format;
+export default pkgToolsConfig.format;